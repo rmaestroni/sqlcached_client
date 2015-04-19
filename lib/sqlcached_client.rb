f15a9184536d071fa72f713c110843e71fe0c29b
Roberto Maestroni <roberto.maestroni@dawandamail.com>
2015-04-20 01:29:43 +0200

    Added has_many/has_one associations

--- lib/sqlcached_client.rb
+++ lib/sqlcached_client.rb
@@ -1,2 +1,5 @@
+require 'active_support'
+require 'active_support/core_ext/string'
+
 require 'sqlcached_client/version'
 require 'sqlcached_client/entity'
--- lib/sqlcached_client/entity.rb
+++ lib/sqlcached_client/entity.rb
@@ -45,6 +45,39 @@ module SqlcachedClient
       def where(params)
         Resultset.new(self, server.run_query(query_id, query, params))
       end
+
+      def has_many(accessor_name, options)
+        foreign_class_name =
+          if options[:class_name].present?
+            options[:class_name]
+          else
+            accessor_name.to_s.singularize.camelize
+          end
+        # get the attributes to define the foreign scope
+        join_attributes = (options[:where] || []).to_a
+        # define the accessor method
+        define_method(accessor_name) do
+          # get the associated entity class
+          foreign_entity = Module.const_get(foreign_class_name)
+          # memoized the associated resultset
+          memoize_var = "@has_many_#{accessor_name}"
+          instance_variable_get(memoize_var) ||
+            instance_variable_set(
+              memoize_var,
+              foreign_entity.where(Hash[ join_attributes.map do |attr_names|
+                [ attr_names[0], send(attr_names[1]) ]
+              end ])
+            )
+        end
+      end
+
+      def has_one(accessor_name, options)
+        plural_accessor_name = accessor_name.to_s.pluralize
+        has_many(plural_accessor_name, options)
+        define_method(accessor_name) do
+          send(plural_accessor_name).first
+        end
+      end
     end # class << self
 
     def to_s
