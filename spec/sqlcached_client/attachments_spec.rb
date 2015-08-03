9f479dff4629d434f137b0f6d84d705aff1431b6
Roberto Maestroni <roberto.maestroni@dawandamail.com>
2015-08-03 13:01:21 +0200

    Specs for Attachments module

--- spec/sqlcached_client/attachments_spec.rb
+++ spec/sqlcached_client/attachments_spec.rb
@@ -1,5 +1,19 @@
 require 'sqlcached_client/attachments'
 
 describe SqlcachedClient::Attachments do
-  pending
+  let(:klass) do
+    Class.new do
+      include SqlcachedClient::Attachments
+
+      has_attachment :foo do
+      end
+    end
+  end
+
+  context "after including the module" do
+    it "add methods 'has_attachment' and 'build_attachments' to the class" do
+      expect(klass.respond_to?(:has_attachment)).to eq(true)
+      expect(klass.respond_to?(:build_attachments)).to eq(true)
+    end
+  end
 end
